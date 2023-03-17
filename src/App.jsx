42f224db9eb97683d2853be912a66859e5bcf5e9
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-17 14:49:03 -0600

    135 - alternativa a useCallback
    Fuera del componente y de la lógica de React

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+
+const fetchData = async (setUsers) => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  setUsers(data);
+};
 
 const App = () => {
   const [counter, setCounter] = useState(0);
   const [users, setUsers] = useState([]); // puede ser null, pero se debe agregar una condición
 
-  const fetchData = useCallback(async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await res.json();
-
-    setUsers(data);
-  }, []);
-
   useEffect(() => {
-    fetchData();
+    fetchData(setUsers);
   }, []);
 
   /** Aquí condición si el useState de users se inicializa null */
